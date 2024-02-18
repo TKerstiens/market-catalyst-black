ca9a63987302df55022d9615aec0a6eba0a2f87a
Tim Kerstiens <tim@kerstiens.io>
2024-02-17 20:38:44 -0600

    Added basic Auth with Token

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../Authentication/AuthContext'; // Adjust the path as needed to import useAuth
+import axios from 'axios';
 
-const Login = () => {
+function Login() {
+  const platformAPIEndpoint = process.env.REACT_APP_PLATFORM_API_URL;
   const { isLoggedIn, login } = useAuth();
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,15 +14,61 @@ const Login = () => {
     }
   }, [isLoggedIn, navigate]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform authentication here (e.g., check credentials)
-    login(); // Update login state on successful authentication
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); // State for storing the error message
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submit action
+
+    try {
+      const response = await axios.post(`${platformAPIEndpoint}/user/auth`, {
+        username,
+        password,
+      });
+
+      login(); //response.data
+      navigate('/'); // Redirect after successful login
+      setErrorMessage(''); // Reset error message state in case of successful login
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        // Extract and display the error message from the response body
+        setErrorMessage(error.response.data.error);
+      } else {
+        console.error('There was an error with the Axios request:', error);
+        setErrorMessage('An unexpected error occurred.'); // Fallback error message
+      }
+    }
   };
 
   return (
-    <button type="button" onClick={handleSubmit}>Login</button>
+    <div>
+      {errorMessage && <div className="error-box">{errorMessage}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
-};
+}
 
 export default Login;
